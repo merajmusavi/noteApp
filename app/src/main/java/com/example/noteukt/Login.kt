a5a67d0cfee603786484b2d77e5560d59c44abe4
merajuni7ed <merajmusavi1243m@gmail.com>
2023-05-08 12:00:52 +0330

    some update about sharePrefrences

--- app/src/main/java/com/example/noteukt/Login.kt
+++ app/src/main/java/com/example/noteukt/Login.kt
@@ -1,6 +1,7 @@
 package com.example.noteukt
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,17 +11,29 @@ import com.example.noteukt.databinding.ActivityMainBinding
 
 class Login : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    lateinit var prefrences:SharedPreferences
+    lateinit var editor:SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        prefrences = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this)
+        editor = prefrences.edit()
+        editor.apply()
+
         if (binding.editTextTextPersonName.text.isEmpty() || binding.editTextpssword.text.isEmpty()) {
+
             Toast.makeText(this, "لطفا مقادیر مد نظر را پر کنید", Toast.LENGTH_SHORT).show()
         } else {
 
             binding.btnLogin.setOnClickListener {
+                val user = binding.editTextTextPersonName.text.toString()
+                val password = binding.editTextpssword.text.toString()
+                editor.putString("username",user).apply()
+                editor.putString("pass",password).apply()
                 var intent = Intent(this, MainActivity::class.java)
+
                 intent.putExtra("name", "meraj")
                 startActivity(intent)
             }
--- app/src/main/java/com/example/noteukt/MainActivity.kt
+++ app/src/main/java/com/example/noteukt/MainActivity.kt
@@ -3,16 +3,27 @@ package com.example.noteukt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.noteukt.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding:ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val intent = intent
-        Toast.makeText(this,intent.getStringExtra("name").toString() , Toast.LENGTH_SHORT).show()
+
+        binding.floatingActionButton.setOnClickListener {
+            showDialog()
+        }
+    }
+
+    fun showDialog() {
+        val view: View = LayoutInflater.from(this).inflate(R.layout.add_note_dialog, null)
+        AlertDialog.Builder(this).setView(view).create().show()
     }
 }
