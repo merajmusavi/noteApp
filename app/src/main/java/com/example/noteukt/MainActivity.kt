cfca64ea20e0b969bc14ce6af44762a742d9639d
merajuni7ed <merajmusavi1243m@gmail.com>
2023-05-12 21:23:07 +0330

    update

--- app/src/main/java/com/example/noteukt/MainActivity.kt
+++ app/src/main/java/com/example/noteukt/MainActivity.kt
@@ -3,12 +3,15 @@ package com.example.noteukt
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.noteukt.databinding.ActivityMainBinding
+import com.example.noteukt.databinding.AddNoteDialogBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -22,15 +25,29 @@ class MainActivity : AppCompatActivity() {
             showDialog()
         }
 
-        binding.rec.layoutManager = LinearLayoutManager(this)
-
-        binding.rec.adapter = RecyclerAdapter(this)
-
 
     }
 
     fun showDialog() {
-        val view: View = LayoutInflater.from(this).inflate(R.layout.add_note_dialog, null)
-        AlertDialog.Builder(this).setView(view).create().show()
+        val dialogBinding: AddNoteDialogBinding =
+            AddNoteDialogBinding.inflate(LayoutInflater.from(this))
+
+        val alert = AlertDialog.Builder(this).setView(dialogBinding.root).create()
+
+        binding.rec.layoutManager = LinearLayoutManager(this)
+        val data = mutableListOf<DataModel>()
+
+        val adapter = RecyclerAdapter(this, data)
+        binding.rec.adapter = adapter
+
+        dialogBinding.btnConfirm.setOnClickListener {
+            data.add(DataModel(dialogBinding.editTextTextPersonName.text.toString(), ""))
+
+
+        }
+
+        alert.show()
+
+
     }
 }
