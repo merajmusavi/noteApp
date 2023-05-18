ef95b5a260bb20978a3dfa769c6d825f71569cbf
merajuni7ed <merajmusavi1243m@gmail.com>
2023-05-18 17:05:43 +0330

    DataBase update

--- app/src/main/java/com/example/noteukt/DataBase/DataBase.kt
+++ app/src/main/java/com/example/noteukt/DataBase/DataBase.kt
@@ -1,4 +1,16 @@
 package com.example.noteukt.DataBase
 
-abstract class DataBase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+@Database(entities = [DataModel::class], version = 1)
+abstract class DataBase() : RoomDatabase {
+abstract fun getNotes():NotesDao
+
+fun createDataBase(context:Context){
+    Room.databaseBuilder(context.applicationContext,DataBase::class.java,"noteDb.db")
+        .allowMainThreadQueries()
+        .build()
+}
 }
