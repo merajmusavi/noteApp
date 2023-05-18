f84ec708ad5ffa3b1dcfa042d3bb5e5d12a4625b
merajuni7ed <merajmusavi1243m@gmail.com>
2023-05-18 13:09:04 +0330

    DataBase update

--- app/src/main/java/com/example/noteukt/DataBase/DataModel.kt
+++ app/src/main/java/com/example/noteukt/DataBase/DataModel.kt
@@ -1,4 +1,19 @@
 package com.example.noteukt.DataBase
 
-data class DataModel(val title:String,val description:String)
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes")
+data class DataModel(
+
+    @PrimaryKey
+    val id: Int,
+
+    @ColumnInfo
+    val title: String,
+
+    @ColumnInfo
+    val description: String
+)
 
