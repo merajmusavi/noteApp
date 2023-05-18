7e02a867da54769431086112722fb33055a61912
merajuni7ed <merajmusavi1243m@gmail.com>
2023-05-18 20:57:58 +0330

    add note activity Update

--- app/src/main/java/com/example/noteukt/Activity/AddNotesActivity.kt
+++ app/src/main/java/com/example/noteukt/Activity/AddNotesActivity.kt
@@ -3,6 +3,7 @@ package com.example.noteukt.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import com.example.noteukt.DataBase.DataModel
 import com.example.noteukt.databinding.ActivityAddNotesBinding
 
 class AddNotesActivity : AppCompatActivity() {
@@ -11,6 +12,13 @@ class AddNotesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAddNotesBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        val title = binding.title.text.toString()
+        val desc = binding.desc.text.toString()
+
+        val notes = mutableListOf<DataModel>()
+
+        notes.add(DataModel(title=title,description=desc))
 
 
     }
--- app/src/main/java/com/example/noteukt/DataBase/DataModel.kt
+++ app/src/main/java/com/example/noteukt/DataBase/DataModel.kt
@@ -8,7 +8,7 @@ import androidx.room.PrimaryKey
 data class DataModel(
 
     @PrimaryKey
-    val id: Int,
+    val id: Int=0,
 
     @ColumnInfo(name = "title")
     val title: String,
